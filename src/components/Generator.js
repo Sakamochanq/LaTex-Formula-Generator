6061e2ddbcd91c2c49342cbcffdfe02f5227967c
Sakamochanq <Sakamochan.q@outlook.jp>
2025-01-01 18:24:14 +0900

    feat: Generate機能の追加

--- src/components/Generator.js
+++ src/components/Generator.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react';
 
 const Generator = () => {
+  const [latex, setLatex] = useState('x = {-b \\pm \\sqrt{b^2 - 4ac} \\over 2a}');
+  const previewRef = useRef(null);
+
+  const handleChange = (e) => {
+    setLatex(e.target.value);
+  };
+
+  useEffect(() => {
+    if (window.MathJax) {
+      window.MathJax.typesetPromise([previewRef.current]).catch(err => console.error(err));
+    }
+  }, [latex]);
+
   return (
     <div className="latex-container">
-        <textarea id="inputBox" >Hello latex</textarea>
-        <div className="previewBox"></div>
+      <textarea value={latex} id="inputBox" onChange={handleChange} />
+      <div className="preview-wrapper">
+        <span className='LaTex'  ref={previewRef} dangerouslySetInnerHTML={{ __html: `\\(${latex}\\)` }} />
+      </div>
     </div>
-  )
-}
-
-export default Generator
+  );
+};
+export default Generator;

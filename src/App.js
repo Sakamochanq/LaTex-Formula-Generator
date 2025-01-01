d3033af0898a4db513679bb0bef9701c2c2d6a64
Sakamochanq <Sakamochan.q@outlook.jp>
2025-01-01 15:14:05 +0900

    feat: とりあえずHeaderComponentの作成

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h2>Hello World</h2>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <header>
+      <h3 className="nav-logo">FrmGen</h3>
+      <nav>
+        <ul className='menu'>
+          <li className='menu-item'><a href="#">Home</a></li>
+          <li className='menu-item'><a href="https://github.com/Sakamochanq/LaTex-Formula-Generator/issues" target='_blank'>Help</a></li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+export default Header

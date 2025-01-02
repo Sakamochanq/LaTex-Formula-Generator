580b7b448c88e66f4ac34ce44b5963889e6611c0
Sakamochanq <Sakamochan.q@outlook.jp>
2025-01-02 12:30:28 +0900

    feat: SelectBoxの追加

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,17 @@ import React from 'react'
 export const Button = () => {
   return (
     <div className="button-container">
-        <button>Click me</button>
+        <label className="selectbox-2">
+            <select>
+                <option>文字・記号</option>
+                <option>演算子</option>
+                <option>論理</option>
+                <option>指数</option>
+                <option>総和</option>
+                <option>集合</option>
+                <option>行列</option>
+            </select>
+        </label>
     </div>
   )
 }

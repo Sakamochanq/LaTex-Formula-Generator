0c7039d124eb32f935f3110428c6d33c78255e99
Sakamochanq <Sakamochan.q@outlook.jp>
2025-01-05 01:53:47 +0900

    remove: 作業しづらいから一端削除

--- src/components/Button.js
+++ src/components/Button.js
@@ -52,70 +52,40 @@ export const Button = () => {
           </label>
         </div>
 
+        {/* 文字・記号 */}
         {selectedIndex === 0 &&
             <div className="button-wrapper">
-                <button>ローマン体</button>
-                <button>ボールド体</button>
-                <button>イタリック体</button>
-                <button>サンセリフ体</button>
-                <button>白抜き文字</button>
             </div>}
 
         {/* 演算子 */}
         {selectedIndex === 1 && 
-            <div className="button-wrapper">    
-                <button>加算</button>
-                <button>減算</button>
-                <button onClick={() => Toggle('\\times')}>掛け算</button>
-                <button>割り算</button>
-                <button>積分</button>          
+            <div className="button-wrapper">      
             </div>}
 
         {/* 論理 */}
         {selectedIndex === 2 && 
             <div className="button-wrapper">
-                <button>論理積</button>
-                <button>論理和</button>
-                <button>否定</button>
-                <button>合意</button>
-                <button>同値</button>
             </div>}
 
         {/* 指数 */}
         {selectedIndex === 3 && 
-            <div className="button-wrapper"> 
-                <button>ローマン体</button>
-                <button>ボールド体</button>
-                <button>イタリック体</button>
-                <button>サンセリフ体</button>
-                <button>白抜き文字</button>
+            <div className="button-wrapper">
             </div>}
 
         {/* 総和 */}
         {selectedIndex === 4 && 
-            <div className="button-wrapper">  
-                <button>指数</button>
-                <button>平方根</button>
-                <button>自然対数</button>
-                <button>対数</button>
+            <div className="button-wrapper">
             </div>}
 
         {/* 集合 */}
         {selectedIndex === 5 && 
-            <div className="button-wrapper">  
-                <button>記号</button>
-                <button>積分記号</button>
-                <button>合計</button>
-                <button>積分</button>
+            <div className="button-wrapper">
+            
             </div>}
 
         {/* 行列 */}
         {selectedIndex === 6 && 
-            <div className="button-wrapper"> 
-                <button>集合記号</button>
-                <button>交差</button>
-                <button>和集合</button>
-                <button>補集合</button>
+            <div className="button-wrapper">
             </div>}
 
       </div>
